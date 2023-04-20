76ecbe639faaecd74081c46dc5817b0d091d567a
aiotlab-teaching <ktlai@arbor.ee.ntu.edu.tw>
2023-04-20 19:34:44 +0800

    Add Java files

---
+++ WeatherData.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class WeatherData implements Subject {
+	private List<Observer> observers;
+	private float temperature;
+	private float humidity;
+	private float pressure;
+	
+	public WeatherData() {
+		observers = new ArrayList<Observer>();
+	}
+	
+	public void registerObserver(Observer o) {
+		System.out.println("Please remove this line and implement registerObserver()");
+	}
+	
+	public void removeObserver(Observer o) {
+		System.out.println("Please remove this line and implement removeObserver()");
+	}
+	
+	public void notifyObservers() {
+		System.out.println("Please remove this line and implement notifyObservers()");
+	}
+	
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+	
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+	
+	public float getHumidity() {
+		return humidity;
+	}
+	
+	public float getPressure() {
+		return pressure;
+	}
+
+}
